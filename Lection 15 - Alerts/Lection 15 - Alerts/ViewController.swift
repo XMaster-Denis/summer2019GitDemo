5752fdb86abc8e013190aecdac7bb89f2a04da39
XMaster <xmaster.denis@gmail.com>
2023-09-15 09:16:06 +0600

    added project

---
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lection 15 - Alerts
+//
+//  Created by XMaster on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
