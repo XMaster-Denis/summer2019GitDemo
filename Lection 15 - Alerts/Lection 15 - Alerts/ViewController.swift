01fd021eaa758420c47fef45feb2050f04bf511c
XMaster <xmaster.denis@gmail.com>
2023-09-15 11:02:14 +0600

    added alert

--- Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -14,6 +14,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you allow to use your photos?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default){_ in
+            print("Permission received")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){_ in
+            print("Permission  not recieved")
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func showActionSheet(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you allow to use your photos?", preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: "OK", style: .default){_ in
+            print("Permission received")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){_ in
+            print("Permission  not recieved")
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
 }
 
